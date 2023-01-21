9e5caeb5cd682b06fbd19705ad927ea6dd7fbb85
Hamza Farahat <farahat.hamza1@gmail.com>
2023-01-21 23:05:51 +0000

    reinitialiser quitter buttons Fixed

--- app/src/main/java/com/example/ex02/MainActivity.java
+++ app/src/main/java/com/example/ex02/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.ex02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnReinitialiser, btnAfficher, btnQuitter;
+    TextView result;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReinitialiser = findViewById(R.id.btnReinitialiser);
+        btnQuitter = findViewById(R.id.btnQuitter);
+        result = findViewById(R.id.result);
+
+        btnReinitialiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("");
+            }
+        });
+
+        btnQuitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
